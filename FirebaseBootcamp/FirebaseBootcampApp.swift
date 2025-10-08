615316eb8e8ca0acc38dd28a997c186bd7b26a52
alena <kellnerovaajka@gmail.com>
2025-10-08 12:44:36 +0200

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Heimdal Data on 08.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseBootcampTests/FirebaseBootcampTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampTests.swift
+//  FirebaseBootcampTests
+//
+//  Created by Heimdal Data on 08.10.2025.
+//
+
+import Testing
+@testable import FirebaseBootcamp
+
+struct FirebaseBootcampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
